2c31ca19f33a2975df7c0ff513d06c564f501e18
shivagopala <159428325+shivagopala@users.noreply.github.com>
2024-02-19 19:56:19 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+
+    struct SIPReturn {
+        uint256 duration;
+        uint256 sipAmount;
+        uint256 futureValue;
+    }
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
@@ -56,5 +60,34 @@ contract Assessment {
 
         // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    // SIP Calculator function
+    function calculateSIPReturn(uint256 monthlyInvestmentAmount, uint256 investmentPeriodYears, uint256 expectedAnnualReturns) public pure returns (uint256) {
+        uint256 totalMonths = investmentPeriodYears * 12;
+        uint256 futureValue = 0;
+        uint256 monthlyInterestRate = expectedAnnualReturns / 12 / 100;
+
+        for (uint256 i = 1; i <= totalMonths; i++) {
+            futureValue += monthlyInvestmentAmount * ((1 + monthlyInterestRate) ** i);
+        }
+
+        return futureValue;
+    }
+
+    // Function to show projected returns for various time durations
+    function getProjectedReturns() public pure returns (SIPReturn[] memory) {
+        SIPReturn[] memory projectedReturns = new SIPReturn[](8);
+
+        projectedReturns[0] = SIPReturn(1, 100, 0);
+        projectedReturns[1] = SIPReturn(5, 100, 100000);
+        projectedReturns[2] = SIPReturn(8, 100, 200000);
+        projectedReturns[3] = SIPReturn(10, 100, 200000);
+        projectedReturns[4] = SIPReturn(12, 100, 300000);
+        projectedReturns[5] = SIPReturn(15, 100, 500000);
+        projectedReturns[6] = SIPReturn(18, 100, 800000);
+        projectedReturns[7] = SIPReturn(20, 100, 1000000);
+
+        return projectedReturns;
     }
 }
